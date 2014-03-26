b36aaac2ea848a87ee93f3ab577d7235cd427bc5
sacry- <nitsche25@gmail.com>
2014-03-27 00:00:09 +0100

    ArrayBuffer Version

--- Scala/a2.scala
+++ Scala/a2.scala
@@ -3,11 +3,21 @@ import scala.annotation.tailrec;
 // Tail Recursive Optimized Array Version..
 def collatz(n: Int): Array[Int] = {
 	@tailrec def collatz_tail(x: Int, accu: Array[Int]): Array[Int] = x match {
-    case 1 => accu :+ 1
+    case 1 => accu :+ 1 // O(?)
     case x if x % 2 == 0 => collatz_tail(x / 2, accu :+ x)
     case _ => collatz_tail(3 * x + 1, accu :+ x)
   }
   collatz_tail(n, Array())
+}
+
+// Tail Recursive Optimized ArrayBuffer Version..
+def collatz_arr_buffer(n: Int): Array[Int] = {
+  @tailrec def collatz_tail(x: Int, accu: ArrayBuffer[Int]): ArrayBuffer[Int] = x match {
+    case 1 => accu += 1 // O(1)
+    case x if x % 2 == 0 => collatz_tail(x / 2, accu += x)
+    case _ => collatz_tail(3 * x + 1, accu += x)
+  }
+  collatz_tail(n, new ArrayBuffer[Int]).toArray
 }
 
 // Simple List Recursion

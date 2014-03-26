573ad2e3c0910a6f4b3f7cbd60d92a54e13eefad
sacry- <nitsche25@gmail.com>
2014-03-26 23:46:03 +0100

    a2...

---
+++ Scala/a2.scala
@@ -0,0 +1,19 @@
+import scala.annotation.tailrec;
+
+// Tail Recursive Optimized Array Version..
+def collatz(n: Int): Array[Int] = {
+	@tailrec def collatz_tail(x: Int, accu: Array[Int]): Array[Int] = x match {
+    case 1 => accu :+ 1
+    case x if x % 2 == 0 => collatz_tail(x / 2, accu :+ x)
+    case _ => collatz_tail(3 * x + 1, accu :+ x)
+  }
+  collatz_tail(n, Array())
+}
+
+// Simple List Recursion
+def collatz_rec(n: Int): List[Int] = n match {
+    case 1 => List(1)
+    case n if n % 2 == 0 => n :: collatz_rec(n / 2)
+    case _ => n :: collatz_rec(3 * x + 1)
+  }
+}
